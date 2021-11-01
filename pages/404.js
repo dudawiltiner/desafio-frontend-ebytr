4c9602641220c3f1f9d47962c092113c9ef0b12e
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-11-01 14:44:49 -0300

    Criado o componente de 404 para a url digitada de form errada  estático

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-depth */
+/* eslint-disable max-len */
 import React from 'react';
 
 export default function Custom404() {
@@ -18,9 +20,8 @@ export default function Custom404() {
           <div className="mb-8 text-lg dark:text-gray-200">
             Você deve ter digitado o endereço errado ou a página foi deslocada.
           </div>
-          <a href="/" className="flex items-center dark:text-gray-100">
-            Ir para Home
-            {' '}
+          <a href="/" className="flex items-center space-x-1 text-lg text-blue-color dark:text-gray-100">
+            <p><b>Ir para Home</b></p>
             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
